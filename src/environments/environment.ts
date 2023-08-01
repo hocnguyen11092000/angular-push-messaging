8d8f2967410bf32aa4cdcb7633216809860ef614
ADMIN\Admin <hocnguyen1109200@gmail.com>
2023-08-01 22:25:11 +0700

    update enviroment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: 'AIzaSyCkYqZ5bHBAinPu-ABha0ijvuqc0xIw-kk',
     authDomain: 'angular-messaging-44560.firebaseapp.com',
