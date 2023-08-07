e174b298b51f489133b2d2c7e3d79f6522861ffd
ADMIN\Admin <hocnguyen1109200@gmail.com>
2023-08-07 21:32:45 +0700

    updaate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,32 +11,32 @@ export class AppComponent implements OnInit {
   title = 'angular-fire-base-push-messaging';
 
   ngOnInit(): void {
-    if (environment.production) {
-      if ('serviceWorker' in navigator) {
-        // Register a service worker hosted at the root of the
-        // site using the default scope.
-        navigator.serviceWorker
-          .register('./static/firebase-messaging-sw.js', {
-            scope: '/',
-          })
-          .then(
-            (registration) => {
-              console.log(
-                'Service worker registration succeeded:',
-                registration
-              );
-              // navigator.serviceWorker.register('/firebase-messaging-sw.js', {
-              //   scope: 'redemption',
-              // });
-            },
-            (error) => {
-              console.error(`Service worker registration failed: ${error}`);
-            }
-          );
-      } else {
-        console.error('Service workers are not supported.');
-      }
-    }
+    // if (environment.production) {
+    //   if ('serviceWorker' in navigator) {
+    //     // Register a service worker hosted at the root of the
+    //     // site using the default scope.
+    //     navigator.serviceWorker
+    //       .register('./static/firebase-messaging-sw.js', {
+    //         scope: '/',
+    //       })
+    //       .then(
+    //         (registration) => {
+    //           console.log(
+    //             'Service worker registration succeeded:',
+    //             registration
+    //           );
+    //           // navigator.serviceWorker.register('/firebase-messaging-sw.js', {
+    //           //   scope: 'redemption',
+    //           // });
+    //         },
+    //         (error) => {
+    //           console.error(`Service worker registration failed: ${error}`);
+    //         }
+    //       );
+    //   } else {
+    //     console.error('Service workers are not supported.');
+    //   }
+    // }
 
     const app = initializeApp(environment.firebaseConfig);
 
